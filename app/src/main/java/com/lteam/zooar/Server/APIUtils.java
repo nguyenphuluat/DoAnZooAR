4eab2701e9b65c2a7bbf22a31ed95831dc2d8b9e
nguyenphuluat <41183982+nguyenphuluat@users.noreply.github.com>
2020-05-05 22:33:27 +0700

    final

--- app/src/main/java/com/lteam/zooar/Server/APIUtils.java
+++
@@ -1,8 +0,0 @@
-package com.lteam.zooar.Server;
-
-public class APIUtils {
-    public static final String base_url="http://192.168.1.5/ARSceneformServer/";
-    public static DataClient getData(){
-        return ReClient.getRetrofit(base_url).create(DataClient.class);
-    }
-}
